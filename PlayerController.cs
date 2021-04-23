463ab443b1aa5070ff4fe11ce4bef3c2b8b0ac40
maxipoemie <44191952+maxipoemie@users.noreply.github.com>
2021-04-23 11:13:46 +0200

    Add files via upload

---
+++ PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 20.0f;
+    public float turnSpeed;
+    private float horizontalInput;
+    private float forwardInput;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        // Move the vehicle forward based on the vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // Move the vehicle forward based on the horizontal input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+    }
+}
